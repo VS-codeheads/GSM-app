57092b105adfa185fc985be3b27f58e3ea476dd9
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2025-12-14 15:21:32 +0100

    conftest file recreated

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,5 +1,25 @@
 import pytest
+from playwright.sync_api import sync_playwright
+
+BASE_URL = "http://127.0.0.1:8000"
 
 @pytest.fixture(scope="session")
-def base_url():
-    return "http://127.0.0.1:8000"
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,  # change to False locally if needed to get UI visually
+            slow_mo=50      # helps to debug flakiness
+        )
+        yield browser
+        browser.close()
+
+
+@pytest.fixture
+def page(browser):
+    context = browser.new_context(
+        viewport={"width": 1280, "height": 800}
+    )
+    page = context.new_page()
+    page.goto(f"{BASE_URL}/index.html")
+    yield page
+    context.close()
